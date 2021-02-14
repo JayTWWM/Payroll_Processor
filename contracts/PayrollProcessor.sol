40afbe05327ee876fda8934561599fadd948c73c
Jay <jaymehta262001@gmail.com>
2021-02-14 05:41:02 +0530

    Seventh

--- contracts/PayrollProcessor.sol
+++ contracts/PayrollProcessor.sol
@@ -218,36 +218,3 @@ contract PayrollProcessor {
     event JoinJob(string email,string profile);
     event TransferMoney(string email_receiver,string email_sender,uint amount,string purpose,string currency);
 }
-
-// contract EliteTokenSale {
-//     address admin;
-    // EliteToken public tokenContract;
-    // uint256 public tokenPrice;
-//     uint256 public tokensSold;
-
-//     event Sell(address _buyer, uint256 _amount);
-
-//     constructor (EliteToken _tokenContract, uint256 _tokenPrice) public {
-//         admin = msg.sender;
-//         tokenContract = _tokenContract;
-//         tokenPrice = _tokenPrice;
-//     }
-
-//     function multiply(uint x, uint y) internal pure returns (uint z) {
-//         require(y == 0 || (z = x * y) / y == x);
-//     }
-
-//     function buyTokens(uint256 _numberOfTokens) public payable {
-//         require(msg.value == multiply(_numberOfTokens, tokenPrice));
-//         require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
-//         require(tokenContract.transfer(msg.sender, _numberOfTokens));
-//         tokensSold += _numberOfTokens;
-//         emit Sell(msg.sender, _numberOfTokens);
-//     }
-
-//     function endSale() public {
-//         require(msg.sender == admin);
-//         require(tokenContract.transfer(admin, tokenContract.balanceOf(address(this))));
-//         address(uint160(admin)).transfer(address(this).balance);
-//     }
-// }
